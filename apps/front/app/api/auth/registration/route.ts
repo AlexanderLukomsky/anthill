b942e9e64d366152478119e04368bd4f127e3a36
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-06-15 18:42:17 +0300

    fix: env dependency

--- apps/front/app/api/auth/registration/route.ts
+++ apps/front/app/api/auth/registration/route.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-
 type RegistrationBody = {
   email: string;
   userName: string;
   password: string;
 };
+
+const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
   const body: RegistrationBody = await request.json();
