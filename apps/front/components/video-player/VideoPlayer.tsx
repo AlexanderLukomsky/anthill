354e1471d0c87e444d3a02546c7d35a134d99fbd
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-06-25 17:35:44 +0300

    feat: fscreen, play icons

--- apps/front/components/video-player/VideoPlayer.tsx
+++ apps/front/components/video-player/VideoPlayer.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Box, Slider, Stack, Typography } from '@mui/material';
+import { Box, IconButton, Slider, Stack, Typography } from '@mui/material';
 import { Button } from 'packages.inputs.button';
 import { FC, useEffect, useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { OnProgressProps } from 'react-player/base';
 import screenfull from 'screenfull';
+import { IconPlay, MaximizeIcon, MinimizeIcon } from './Icons';
 
 export type VideoPlayerProps = {};
 
@@ -24,6 +25,9 @@ export const VideoPlayer: FC<VideoPlayerProps> = () => {
 
   const handleFullScreenMode = () => {
     screenfull.toggle(containerPlayerRef.current);
+    screenfull.onchange(() => {
+      setRender((prev) => prev + 1);
+    });
   };
 
   const handlePlayerProgress = (player: OnProgressProps) => {
@@ -43,10 +47,14 @@ export const VideoPlayer: FC<VideoPlayerProps> = () => {
       setHasWindow(true);
     }
   }, []);
+  console.log(screenfull.isFullscreen);
 
   return (
     <Stack sx={{ background: '#404040' }}>
-      <Box ref={containerPlayerRef} sx={{ maxWidth: '1250px', width: '100%', height: '700px' }}>
+      <Box
+        ref={containerPlayerRef}
+        sx={{ maxWidth: '1250px', width: '100%', height: '100vh', position: 'relative' }}
+      >
         {hasWindow && (
           <ReactPlayer
             width="100%"
@@ -59,51 +67,63 @@ export const VideoPlayer: FC<VideoPlayerProps> = () => {
             onProgress={handlePlayerProgress}
           />
         )}
+        <Stack
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            zIndex: 2,
+          }}
+        >
+          <IconButton onClick={handleFullScreenMode}>
+            {screenfull.isFullscreen && <MinimizeIcon />}
+            {!screenfull.isFullscreen && <MaximizeIcon />}
+          </IconButton>
+
+          {!playing && (
+            <IconButton onClick={() => setPlaying(true)}>
+              <IconPlay />
+            </IconButton>
+          )}
+          {playing && <Button onClick={() => setPlaying(false)}>pause</Button>}
+          {!muted && <Button onClick={() => setMuted(true)}>muted</Button>}
+          {muted && <Button onClick={() => setMuted(false)}>unmuted</Button>}
+
+          <Box sx={{ height: '150px', position: 'absolute' }}>
+            <Slider
+              orientation="vertical"
+              min={0}
+              max={1}
+              value={volume}
+              step={0.01}
+              size="small"
+              onChange={(event, value) => {
+                setVolume(value as number);
+              }}
+            />
+          </Box>
+          <Box>
+            <Slider
+              min={0}
+              max={movieDuration}
+              value={currentPlayerTime}
+              step={0.01}
+              size="small"
+              onChange={(event, value) => {
+                console.log(value);
+                setCurrentPlayerTime((value as number) + 1);
+                playerRef.current.seekTo((value as number) + 1);
+              }}
+            />
+            <Typography style={{ color: 'white' }}>{playedTime}</Typography>
+            <Typography style={{ color: 'white' }}>{fullMovieTime}</Typography>
+          </Box>
+        </Stack>
       </Box>
-
-      <Stack
-        sx={{
-          background: 'rgba(16, 16, 16, 0.4);',
-
-          color: '#fff',
-        }}
-      >
-        <Button onClick={handleFullScreenMode}>fullscreen</Button>
-        {!playing && <Button onClick={() => setPlaying(true)}>play</Button>}
-        {playing && <Button onClick={() => setPlaying(false)}>pause</Button>}
-        {!muted && <Button onClick={() => setMuted(true)}>muted</Button>}
-        {muted && <Button onClick={() => setMuted(false)}>unmuted</Button>}
-
-        <Box sx={{ height: '150px', position: 'absolute' }}>
-          <Slider
-            orientation="vertical"
-            min={0}
-            max={1}
-            value={volume}
-            step={0.01}
-            size="small"
-            onChange={(event, value) => {
-              setVolume(value as number);
-            }}
-          />
-        </Box>
-        <Box>
-          <Slider
-            min={0}
-            max={movieDuration}
-            value={currentPlayerTime}
-            step={0.01}
-            size="small"
-            onChange={(event, value) => {
-              console.log(value);
-              setCurrentPlayerTime((value as number) + 1);
-              playerRef.current.seekTo((value as number) + 1);
-            }}
-          />
-          <Typography style={{ color: 'white' }}>{playedTime}</Typography>
-          <Typography style={{ color: 'white' }}>{fullMovieTime}</Typography>
-        </Box>
-      </Stack>
     </Stack>
   );
 };
